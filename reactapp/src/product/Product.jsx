aec86befd3f30ed56e7f604f2aa48ef86c2389fa
Iamneo <techadmin@iamneo.ai>
2023-09-20 06:58:26 +0000

    Backup Commit

--- reactapp/src/product/Product.jsx
+++ reactapp/src/product/Product.jsx
@@ -35,7 +35,7 @@ const productsData = [
     name: "Product 5",
     description: "Aenean lacinia bibendum nulla sed consectetur.",
     price: 9.99,
-    image: require(),
+    image: require("./product1.jpg"),
   },
 ];
 
